8d881f09aede2e9c682405a56545e339c9e38f3f
shunhua <gentryhuang.xw@gmail.com>
2020-03-23 22:40:09 +0800

    dubbo filter

--- dubbo/dubbo-spi/provider/src/main/java/com/code/dubbo/spi/provider/filter/DubboExceptionFilter.java
+++ dubbo/dubbo-spi/provider/src/main/java/com/code/dubbo/spi/provider/filter/DubboExceptionFilter.java
@@ -1,11 +1,15 @@
 package com.code.dubbo.spi.provider.filter;
 
 
+import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.*;
+
+import static org.apache.dubbo.common.constants.CommonConstants.PROVIDER;
 
 
 /**
  */
+@Activate(group = PROVIDER)
 public class DubboExceptionFilter implements Filter {
 
 
